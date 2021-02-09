3b109bf35ed7d3c84840d897a2824d62a1bca3b7
cxf213 <cxf213@outlook.com>
2021-02-09 18:11:27 +0800

    分批训练

--- tutorial/classification.py
+++ tutorial/classification.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn.functional as F
 import matplotlib.pyplot as plt
+import  torch.utils.data as Data
 
 # torch.manual_seed(1)    # reproducible
 
@@ -13,8 +14,9 @@ y1 = torch.ones(100)  # class1 y data (tensor), shape=(100, 1)
 x = torch.cat((x0, x1), 0).type(torch.FloatTensor)  # shape (200, 2) FloatTensor = 32-bit floating
 y = torch.cat((y0, y1), ).type(torch.LongTensor)  # shape (200,) LongTensor = 64-bit integer
 
-# The code below is deprecated in Pytorch 0.4. Now, autograd directly supports tensors
-# x, y = Variable(x), Variable(y)
+BATCH_SIZE = 5
+torch_dataset = Data.TensorDataset(x,y)
+loader = Data.DataLoader()
 
 # plt.scatter(x.data.numpy()[:, 0], x.data.numpy()[:, 1], c=y.data.numpy(), s=100, lw=0, cmap='RdYlGn')
 # plt.show()
@@ -32,13 +34,26 @@ class Net(torch.nn.Module):
         return x
 
 
-net = Net(n_feature=2, n_hidden=10, n_output=2)  # define the network
-print(net)  # net architecture
+net = Net(n_feature=2, n_hidden=4, n_output=2)  # define the network
+# print(net)  # net architecture
 
 optimizer = torch.optim.SGD(net.parameters(), lr=0.02)
 loss_func = torch.nn.CrossEntropyLoss()  # the target label is NOT an one-hotted
 
-plt.ion()  # something about plotting
+# plt.ion()  # something about plotting
+
+
+for epoch in range(3):
+    for step, (batch_x, batch_y) in enumerate(loader):
+        out = net(x)  # input x and predict based on x
+        loss = loss_func(out, y)  # must be (1. nn output, 2. target), the target label is NOT one-hotted
+
+        optimizer.zero_grad()  # clear gradients for next train
+        loss.backward()  # backpropagation, compute gradients
+        optimizer.step()  # apply gradients
+        print('Epoch: ', epoch, '| Step: ', step, '| batch x: ',
+              batch_x.numpy(), '| batch y: ', batch_y.numpy())
+
 
 for t in range(100):
     out = net(x)  # input x and predict based on x
@@ -48,16 +63,17 @@ for t in range(100):
     loss.backward()  # backpropagation, compute gradients
     optimizer.step()  # apply gradients
 
-    if t % 2 == 0:
+    if t % 10 == 0:
         # plot and show learning process
-        plt.cla()
+        # plt.cla()
         prediction = torch.max(out, 1)[1]
         pred_y = prediction.data.numpy()
         target_y = y.data.numpy()
-        plt.scatter(x.data.numpy()[:, 0], x.data.numpy()[:, 1], c=pred_y, s=100, lw=0, cmap='RdYlGn')
+        # plt.scatter(x.data.numpy()[:, 0], x.data.numpy()[:, 1], c=pred_y, s=100, lw=0, cmap='RdYlGn')
         accuracy = float((pred_y == target_y).astype(int).sum()) / float(target_y.size)
-        plt.text(1.5, -4, 'Accuracy=%.2f' % accuracy, fontdict={'size': 20, 'color': 'red'})
+        print(accuracy)
+        # plt.text(1.5, -4, 'Accuracy=%.2f' % accuracy, fontdict={'size': 20, 'color': 'red'})
         #plt.pause(0.1)
 
-plt.ioff()
-plt.show()
+# plt.ioff()
+# plt.show()
